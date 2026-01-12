bf3ed6d4096421f11679eb5cb51d3edf595ed27d
SyedAnees2003 <aneesjha3@gmail.com>
2026-01-12 10:18:02 +0530

    Commit for removed localhost dependencies

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,12 @@
 const { Sequelize } = require("sequelize");
 
-let sequelize;
-
-if (process.env.MYSQL_URL) {
-  console.log("📦 Database URL: Using MYSQL_URL from env");
-
-  sequelize = new Sequelize(process.env.MYSQL_URL, {
-    dialect: "mysql",
-    logging: false,
-  });
-} else {
-  console.log("📦 Database URL: Using individual env vars");
-
-  sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE,
-    process.env.MYSQL_USER,
-    process.env.MYSQL_PASSWORD,
-    {
-      host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
-      dialect: "mysql",
-      logging: false,
-    }
-  );
+if (!process.env.MYSQL_URL) {
+  throw new Error("MYSQL_URL is not defined in environment variables");
 }
+
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  logging: false,
+});
 
 module.exports = sequelize;
